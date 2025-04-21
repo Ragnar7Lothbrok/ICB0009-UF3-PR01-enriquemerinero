66bb9e1357650a9db70e2e2c7f087281f1aa9713
Ragnar7Lothbrok <kiqqe.7@gmail.com>
2025-04-21 16:37:45 +0200

    Etapa 1 ejercicio 2

--- NetworkStreamClass/NetworkStreamClass.cs
+++ NetworkStreamClass/NetworkStreamClass.cs
@@ -14,27 +14,40 @@ namespace NetworkStreamNS
         //Método para escribir en un NetworkStream los datos de tipo Carretera
         public static void  EscribirDatosCarreteraNS(NetworkStream NS, Carretera C)
         {            
-                            
+            byte[] datos = C.Serializar();
+            NS.Write(datos, 0, datos.Length);               
         }
 
         //Metódo para leer de un NetworkStream los datos que de un objeto Carretera
-        /*public static Carretera LeerDatosCarreteraNS (NetworkStream NS)
+        public static Carretera LeerDatosCarreteraNS (NetworkStream NS)
         {
-            
+            byte[] buffer = new byte[4096];
+            int bytesLeidos = NS.Read(buffer, 0, buffer.Length);
 
-        }*/
+            byte[] datosLeidos = new byte[bytesLeidos];
+            Array.Copy(buffer, datosLeidos, bytesLeidos);
+
+            return Carretera.Deserializar(datosLeidos);
+        }
 
         //Método para enviar datos de tipo Vehiculo en un NetworkStream
         public static void  EscribirDatosVehiculoNS(NetworkStream NS, Vehiculo V)
         {            
-                              
+            byte[] datos = V.Serializar();
+            NS.Write(datos, 0, datos.Length);                      
         }
 
         //Metódo para leer de un NetworkStream los datos que de un objeto Vehiculo
-        /*public static Vehiculo LeerDatosVehiculoNS (NetworkStream NS)
+        public static Vehiculo LeerDatosVehiculoNS (NetworkStream NS)
         {
+            byte[] buffer = new byte[1024];
+            int bytesLeidos = NS.Read(buffer, 0, buffer.Length);
 
-        }*/
+            byte[] datosLeidos = new byte[bytesLeidos];
+            Array.Copy(buffer, datosLeidos, bytesLeidos);
+
+            return Vehiculo.Deserializar(datosLeidos);
+        }
 
         public static string LeerMensajeNetworkStream (NetworkStream NS)
         {
